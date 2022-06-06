af97099ca40f37ae5fa500d214336f3b2527dc4d
Omotara Morenikeji <omotarakeji@gmail.com>
2022-06-06 11:27:00 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 * main - Prints all possible combinations of single-digit numbers.
-*
+* Numbers must be separated by , followed by a space
 * Return: Always 0.
 */
 int main(void)
